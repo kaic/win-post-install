8d03c29861bd1189f733cc58a6017aba542d2b2e
Kaic Bento <kaicbento@outlook.com>
2025-12-15 17:10:17 -0300

    feat: add intel driver support assistant

---
+++ src/data/software/drivers/intel-driver-support.js
@@ -0,0 +1,12 @@
+export default {
+  "id": "intel-driver-support",
+  "name": "Intel Driver & Support Assistant",
+  "description": "Official Intel tool for updating Intel drivers and firmware",
+  "category": "drivers",
+  "wingetId": "Intel.DriverAndSupportAssistant",
+  "icon": "SiIntel",
+  "iconColor": "#0071C5",
+  "popular": true,
+  "requiresAdmin": true,
+  "license": "free"
+};
